5dfc7356464bd020c19e4836e02d602c9aa9d47b
mgomes-d <murillo.jogos8@hotmail.com>
2024-03-04 10:26:17 +0100

    standarisation of input data using z-score normalisation

--- logreg_train.py
+++ logreg_train.py
@@ -4,17 +4,22 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
-class GradientDescent:
-    def __init_(self):
-        self.x = 0
+# class GradientDescent:
+#     def __init_(self):
+#         self.x = 0
 
 class LogisticRegression:
     def __init__(self, df):
-        self.x = 0
-        self.df = stand_input_data(df)
+        self.df = self.stand_input_data(df)
 
     def stand_input_data(self, df):
-        data = df
+        data = df.copy()
+        self.means = {}
+        self.std = {}
+        for column_name, content in data.drop("Hogwarts House", axis=1, inplace=False).items():
+            self.means[column_name] = 1 / content.size * content.values.sum()
+            self.std[column_name] = (content.apply(lambda x: (x - self.means[column_name])**2).values.sum() / content.size)**0.5
+            data[column_name] = data[column_name].apply(lambda x: (x - self.means[column_name]) / self.std[column_name])
         return data
 
 
@@ -32,12 +37,12 @@ def main():
         # Charger les données
         data_train = load_csv(sys.argv[1])
         data_parsed = parse_data(data_train)
-        logistic_regression = LogisticRegression()
-        print(data_parsed)
+        # print(data_parsed)
+        logistic_regression = LogisticRegression(data_parsed)
         # logisticReg = LogisticRegression(data_parsed)
 
     except Exception as msg:
-        print(msg)
+        print(msg, "ddf")
 
 
 if __name__ == "__main__":
